69210be3ca6ca0ac72591d0bca421c090f679635
tony2020-god <62175480+tony2020-god@users.noreply.github.com>
2020-12-02 02:07:15 +0800

    選單進度80%

---
+++ Escape room/Assets/程式/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+  
+    
+    public GameObject background;
+    public Animator ani;
+
+    public void StartGame()
+    {
+        
+        SceneManager.LoadScene("關卡1");
+    }
+    public void ButtunChick()
+    {
+        print("123");
+        
+        ani.SetBool("點擊開始遊戲", true);
+        Invoke("StartGame", 3f);
+    }
+}
