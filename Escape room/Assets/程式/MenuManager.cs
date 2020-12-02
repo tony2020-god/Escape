2203b3c4cd321384b4fc489e9c6fb9d732c580b1
tony2020-god <62175480+tony2020-god@users.noreply.github.com>
2020-12-02 14:33:58 +0800

    選單完成

--- Escape room/Assets/程式/MenuManager.cs
+++ Escape room/Assets/程式/MenuManager.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;//引用系統集合、管理API(協同程式:非同步處理)
 
 public class MenuManager : MonoBehaviour
 {
@@ -7,17 +9,23 @@ public class MenuManager : MonoBehaviour
     
     public GameObject background;
     public Animator ani;
-
+    public GameObject runtext;
+    public GameObject chicktext;
     public void StartGame()
     {
-        
         SceneManager.LoadScene("關卡1");
     }
-    public void ButtunChick()
+    public IEnumerator ButtunChick()
     {
         print("123");
-        
         ani.SetBool("點擊開始遊戲", true);
+        yield return new WaitForSeconds(0.5f);
+        Destroy(runtext);
+        Destroy(chicktext);
         Invoke("StartGame", 3f);
+    }
+    public void buttonclick()
+    {
+        StartCoroutine(ButtunChick());
     }
 }
